2563d14e26a6505928cce3e829584e9d77fa5e37
bogdanzamfiroiu <bzamfiroiu6@gmail.com>
2024-10-19 14:10:32 +0300

    Working Google Maps

--- app/src/main/java/com/davtheutz/adobeitfestv3/MainActivity.java
+++ app/src/main/java/com/davtheutz/adobeitfestv3/MainActivity.java
@@ -1,5 +1,6 @@
 package com.davtheutz.adobeitfestv3;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -20,5 +21,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        startActivity(new Intent(this, MapsActivity33.class));
     }
 }
